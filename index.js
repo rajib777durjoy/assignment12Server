14906619810ac5d635009ddfb755c3026f78b930
rajib777durjoy <rajib777chanda@gmail.com>
2025-01-19 20:39:17 +0600

    applied details page and applied remove functionality done

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ const varifytoken=(req,res,next)=>{
     }
     req.decoded=decoded;
     next();
-  })
-  
+  }) 
 }
 const uri = `mongodb+srv://${process.env.User_key}:${process.env.User_pass}@cluster0.u87dt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -42,7 +41,8 @@ async function run() {
     const database = client.db("fitnessZone");
     const usersDb = database.collection("userCollection");
     const trainerDb=database.collection('trainerCollection');
-   
+    const allTrainer=database.collection('alltrainerCollection');
+    const BookDetails=database.collection('trainerBooked');
     // token create related api//
     app.post('/jwt',async(req,res)=>{
       const userEmail= req.body;
@@ -50,6 +50,25 @@ async function run() {
       const token=jwt.sign(userEmail,process.env.Access_Token_key, { expiresIn: '1h' });
       res.send({token})
     })
+  // check admin check //
+  app.get('/userCheck/:email',async(req,res)=>{
+    const email= req.params.email;
+    const query={email:email};
+    const result= await usersDb.findOne(query);
+    // console.log(result)
+    let user='member'
+    // if(result.role==="admin"){
+    //    user='admin'
+    // }
+    // else if(result.role==='trainer'){
+    //     user ='trainer'
+    // }
+    // else if(result.role === 'member'){
+    //   user ='member'
+    // }
+    // console.log('user',user)
+    res.send(user)
+  })
  // user related api//
     app.post('/user',varifytoken,async(req,res)=>{
         const userId=req.body;
@@ -61,6 +80,14 @@ async function run() {
    
         res.send(result)
     })
+  /// all newsletter subcriber (admin page) ///
+  app.get('/allnewsletter/:email',async(req,res)=>{
+    const userEmail= req.params.email;
+    const query={email:{$ne:userEmail}}
+    const result = await usersDb.find(query).toArray()
+    console.log(result)
+    res.send(result)
+  })
 // trainer related api //
 app.post('/trainer',async(req,res)=>{
   const data= req.body;
@@ -81,14 +108,86 @@ app.get('/trainerDetails/:id',async(req,res)=>{
   const result= await trainerDb.findOne(query);
   res.send(result)
 })
+/// trainer booked related api //
+app.get('/bookedData/:id',async(req,res)=>{
+     const ids= req.params.id;
+     const query={_id:new ObjectId(ids)}
+     const result = await BookDetails.findOne(query)
+    //  console.log(result)
+     res.send(result)
+})
+// applied trainer (admin page) ///
+app.get('/appliedTrainer/:email',async(req,res)=>{
+  const useremail= req.params.email;
+  const query={email:{$ne:useremail}}
+  const result= await trainerDb.find(query).toArray()
+  // console.log('applied trainer',result)
+  res.send(result)
+})
 
+/// applied details /// todo: all trainer db change///
+app.get('/details/:id',async(req,res)=>{
+  const ids= req.params.id;
+  const query={_id:new ObjectId(ids)}
+  const details= await trainerDb.findOne(query)
+  // console.log(details)
+  res.send(details)
+})
+// applied status update///
+app.patch('/statusChange',async(req,res)=>{
+  const data=req.body;
+  console.log('status',data.status)
+  const query={_id:new ObjectId(data.id)}
+  const update={
+    $set:{
+      status:data.status,
+      role:'trainer'
+    }
+  }
+  const options = { upsert: true };
+  const result= await trainerDb.updateOne(query,update,options)
+  res.send(result)
+})
+/// applied remove ///
+app.delete('/applied/:id',async(req,res)=>{
+  const Id= req.params.id;
+  // console.log(Id)
+  const query={_id:new ObjectId(Id)}
+  const result= await trainerDb.deleteOne(query)
+  console.log('remove',result)
+  res.send(result)
+})
+// add Class (Admin)//
+app.post('/addClass',async(req,res)=>{
+  const data = req.body;
+  const result= await allTrainer.insertOne(data)
+  console.log(data)
+})
+/// set availabel slot ///
+app.post('/slot',async(req,res)=>{
+  const data= req.body;
+  const check={
+    bookId:data.bookId,
+    name:data.name,
+    slot:data.slot
+  }
+  const query=await BookDetails.findOne(check)
+  console.log(query)
+  if(query){
+    return res.send({message:"Your Trainer Booked"})
+  }
+  const result= await BookDetails.insertOne(data);
+ 
+  console.log(result)
+  res.send(result)
+})
 
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
